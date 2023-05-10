4a5bbb2f5969a9bcfa92a9773d162663d4a53fd0
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-10 19:32:57 +0300

    Initial commit

---
+++ src/Draft0_Whole_Video_2_4_0.java
@@ -0,0 +1,33 @@
+//видео здесь : https://lms.synergy.ru/learning/view/68495/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
+
+public class Draft0_Whole_Video_2_4_0 {
+    public static void main(String[] args) {
+
+//        Пример 3 ППППППППППППППППППППППППППППППППППП конструкция switch ... case ; видео мин 27.00
+        /* видео мин 27.00 , https://lms.synergy.ru/learning/view/68495/?groupPeriodId=1045153&disciplineVersionId=25858&start=1 */
+
+//        Конец Примера 3 КККККККККККККККК
+
+
+
+
+
+
+
+//        Пример 2 ППППППППППППППППППППППППППППППППППП Вывести курсы валют за год, вложенный цикл do ... while ; видео мин 20.51
+        /* видео мин 20.51 , https://lms.synergy.ru/learning/view/68495/?groupPeriodId=1045153&disciplineVersionId=25858&start=1 */
+
+//        Конец Примера 2 КККККККККККККККК
+
+
+
+
+
+//        Пример 1 ППППППППППППППППППППППППППППППППППП Вывести делители чисел от 1 до 100, вложенный цикл  ;  видео мин 18.41
+        /* видео мин 18.41 , https://lms.synergy.ru/learning/view/68495/?groupPeriodId=1045153&disciplineVersionId=25858&start=1 */
+
+//        Конец Примера 1 КККККККККККККККК
+
+    }
+
+}
